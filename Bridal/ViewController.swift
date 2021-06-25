5a0e8b700da44aa37ba933c5f2ce16d04445473b
Abeer Hasan <abeerhasan080@gmail.com>
2021-06-25 12:54:15 +0200

    Initial Commit

---
+++ Bridal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bridal
+//
+//  Created by Abeer Hasan on 25/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
